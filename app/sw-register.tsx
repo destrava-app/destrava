40330495d47da1ed56ad98e97a78819ec619d081
destrava-app <brunoreichel@gmail.com>
2025-08-24 15:13:57 -0300

    Delete app directory

--- app/sw-register.tsx
+++
@@ -1,4 +0,0 @@
-'use client';
-import { useEffect } from 'react';
-import { registerSW } from '../src/register-sw';
-export default function SWRegister(){ useEffect(()=>{registerSW();},[]); return null; }
